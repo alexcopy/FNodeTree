9c90bfcdf3398d015d6d76ecede9f19310e62cf3
Alex Redko <redcopy@mail.ru>
2015-12-14 13:11:17 +0000

    Refresh is Done

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,16 +3,20 @@ var fTree = angular.module('fTree', []);
 fTree.controller('AppCtrl', ['$scope', '$http',
   function ($scope, $http) {
     console.log("UFO Traces in Controller");
-
-    $http.get('/contactlist').success(function (response) {
-      console.log('I got data requested');
-      $scope.contactlist = response;
-    });
+    var refresh = function () {
+      $http.get('/contactlist').success(function (response) {
+        console.log('I got data requested');
+        $scope.contactlist = response;
+        $scope.contact = "";
+      });
+    };
+    refresh();
 
     $scope.addContact = function () {
       console.log($scope.contact);
       $http.post('/contactlist', $scope.contact).success(function (response) {
         console.log(response);
+        refresh();
       });
     };
 
