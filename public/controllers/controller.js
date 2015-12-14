b179da3f2aa975a0159f3069a3a64dcb842abc25
Alex Redko <redcopy@mail.ru>
2015-12-14 13:05:40 +0000

    MongoDb setuped (read/write) from app, Controller is getting data and recording to DB. Installation notes created. Added two NPM packages:   - bodyParser   - mongojs

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,21 @@
-function AppCtrl() {
-  console.log("UFO Traces in Controller");
-}
+var fTree = angular.module('fTree', []);
+
+fTree.controller('AppCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    console.log("UFO Traces in Controller");
+
+    $http.get('/contactlist').success(function (response) {
+      console.log('I got data requested');
+      $scope.contactlist = response;
+    });
+
+    $scope.addContact = function () {
+      console.log($scope.contact);
+      $http.post('/contactlist', $scope.contact).success(function (response) {
+        console.log(response);
+      });
+    };
+
+  }]);
+
+
--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-app.listen(3001);
+app.get('/contactlist', function (req, res) {
+  console.log('GET request');
+
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+
+});
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function (err, doc) {
+    res.json(doc);
+  });
+
+
+});
+app.listen(3002);
 console.log('Server is running on port 3001')
